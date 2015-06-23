c8141921cd223cbe008994113e7199ec41630282
Tim Case <tim@2drops.net>
2015-06-23 20:44:39 +0700

    change lib dir structure

---
+++ lib/taxjar/version.rb
@@ -0,0 +1,45 @@
+module Taxjar
+  module Version
+  module_function
+
+    # @return [Integer]
+    def major
+      1
+    end
+
+    # @return [Integer]
+    def minor
+      0
+    end
+
+    # @return [Integer]
+    def patch
+      0
+    end
+
+    # @return [Integer, NilClass]
+    def pre
+      nil
+    end
+
+    # @return [Hash]
+    def to_h
+      {
+        major: major,
+        minor: minor,
+        patch: patch,
+        pre: pre,
+      }
+    end
+
+    # @return [Array]
+    def to_a
+      to_h.values.compact
+    end
+
+    # @return [String]
+    def to_s
+      to_a.join('.')
+    end
+  end
+end
