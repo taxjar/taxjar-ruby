fdc6d3736897ab5365e6ee73f88f7414ecaa7921
Tim Case <tim@2drops.net>
2015-06-23 21:16:41 +0700

    add configuration methods for client

--- lib/taxjar/client.rb
+++ lib/taxjar/client.rb
@@ -2,5 +2,25 @@ module Taxjar
   class Client
     attr_accessor :api_key
 
+    # Initializes a new Client object
+    #
+    # @param options [Hash]
+    # @return [Taxjar::Client]
+    def initialize(options = {})
+      options.each do |key, value|
+        instance_variable_set("@#{key}", value)
+      end
+      yield(self) if block_given?
+    end
+
+    # @return [Boolean]
+    def api_key?
+      !!@api_key
+    end
+
+    # @return [String]
+    def user_agent
+      "TaxjarRubyGem/#{Taxjar::Version}"
+    end
   end
 end
--- spec/taxjar/client_spec.rb
+++ spec/taxjar/client_spec.rb
@@ -1,7 +1,23 @@
 require 'helper'
 
 describe Taxjar::Client do
-  it 'should be true' do
-    expect(true).to be_true
+
+  describe '#api_key?' do 
+    it 'returns true if api_key is present' do
+      client = Taxjar::Client.new(api_key: 'AK')
+      expect(client.api_key?).to be true
+    end
+
+    it 'returns false if api_key is not present' do
+      client = Taxjar::Client.new
+      expect(client.api_key?).to be false
+    end
+  end
+
+  describe '#user_agent' do
+    it 'returns string with version' do
+      client = Taxjar::Client.new(api_key: 'AK')
+      expect(client.user_agent).to eq("TaxjarRubyGem/#{Taxjar::Version}")
+    end
   end
 end
