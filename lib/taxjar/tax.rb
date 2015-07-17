b60f868d9bf4a2362e875bd5e7951c551b0c36d1
Jake Johnson <jake@fastdivision.com>
2015-07-17 16:47:12 -0700

    Fix typo, bump version

--- lib/taxjar/tax.rb
+++ lib/taxjar/tax.rb
@@ -5,7 +5,7 @@ module Taxjar
     attr_reader :order_total_amount, :amount_to_collect, :has_nexus, 
       :freight_taxable, :tax_source
 
-    object_attr_reader Taxjar::Breakdown, :brekdown
+    object_attr_reader Taxjar::Breakdown, :breakdown
 
   end
 end
