1ffc1d8b67790855c90a697ba366a833dbe91683
David DeRemer <david@goposse.com>
2015-09-30 14:30:14 -0400

    Update rate.rb

--- lib/taxjar/rate.rb
+++ lib/taxjar/rate.rb
@@ -2,7 +2,7 @@ require 'taxjar/base'
 
 module Taxjar
   class Rate < Taxjar::Base
-    attr_reader :zip, :state, :state_rate, :county, :city, :city_rate,
+    attr_reader :zip, :state, :state_rate, :county, :county_rate, :city, :city_rate,
       :combined_distric_rate, :combined_rate
 
   end
